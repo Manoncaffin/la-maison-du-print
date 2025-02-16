539873f64dd678df3db8c23086586c0bd261d78b
Caffin <caffinmanon@gmail.com>
2025-02-16 21:02:30 +0100

    gallery

--- site/templates/galerie.php
+++ site/templates/galerie.php
@@ -3,29 +3,34 @@
     <?= snippet('header')
     ?>
 
-
-
     <main class="container_gallery">
         <section class="gallery">
 
-        <div class="tags-desktop desktop-only">
-    <button class="tag-desktop active" data-category="all">tout</button>
-    <button class="tag-desktop" data-category="serigraphie">sérigraphie</button>
-    <button class="tag-desktop" data-category="dtf">impression DTF</button>
-</div>
-
-        <?php foreach ($page->gallery_items()->toStructure() as $item): ?>
-        <?php
-            $categories = $item->category()->split(); // Récupère la catégorie
-            $image = $item->image()->toFile(); // Récupère l'image
-        ?>
-        <?php if ($image): ?>
-            <div class="gallery-item" data-category="<?= implode(' ', $categories) ?>">
-                <img src="<?= $image->url() ?>" alt="<?= $item->about()->esc() ?>">
-                <p><?= $item->about()->esc() ?></p>
+            <div class="tags-desktop desktop-only">
+                <button class="tag-desktop active" data-category="all">TOUT</button>
+                <button class="tag-desktop" data-category="serigraphie">SÉRIGRAPHIE</button>
+                <button class="tag-desktop" data-category="dtf">IMPRESSION DTF</button>
             </div>
-        <?php endif; ?>
-    <?php endforeach; ?>
+
+            <?php 
+$index = 1; // Initialisation du compteur
+?>
+
+<?php foreach ($page->gallery_items()->toStructure() as $item): ?>
+    <?php
+        $categories = $item->category()->split(); // Récupère la catégorie
+        $image = $item->image()->toFile(); // Récupère l'image
+    ?>
+
+    <?php if ($image): ?>
+        <div class="gallery-item column-<?= $index ?>" data-category="<?= implode(' ', $categories) ?>">
+            <img src="<?= $image->url() ?>" alt="<?= $item->about()->esc() ?>">
+            <p><?= $item->about()->esc() ?></p>
+        </div>
+        <?php $index++; // Incrémentation du compteur ?>
+    <?php endif; ?>
+<?php endforeach; ?>
+
         </section>
 
     </main>
